ee9e3ba067273cd8323adfba326892ecde8d55f8
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-19 23:06:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from app.book import Book
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
+        if cmd == "display":
+            book.display(method_type)
         if cmd == "print":
             book.print_book(method_type)
         elif cmd == "serialize":
